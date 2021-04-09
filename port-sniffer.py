87d7a5d458547f94b0d5ea4764e611c7c1311583
erfanemamverdi <erfanemamverdi9235@gmail.com>
2021-04-09 23:04:27 +0430

    implement socket scan type 1 v 2

--- port-sniffer.py
+++ port-sniffer.py
@@ -1,1 +1,37 @@
+import socket
 print("Welcome!!")
+
+def scan(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((host, port))
+        print("Port open: " + str(port))
+        s.close()
+    except:
+        print("Port closed: " + str(port))
+
+print("Enter host:")
+host = input()	
+
+min = 0
+max = 65353
+
+ports = []
+
+print("Enter call type: ")
+print("1. all ports ")
+print("2. reserved ports ")
+print("3. question ")
+type = input()
+
+if type == '1':
+    ports = list(range(min, max + 1))
+if type == '2':
+    print("Enter min port: ")
+    min = int(input())
+    print("Enter max port: ")
+    max = int(input())
+    ports = list(range(min, max + 1))
+
+for port in range(ports):
+    scan(host, int(port))    
