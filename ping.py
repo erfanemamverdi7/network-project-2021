095099f5123a4cd3e7e326b75db1121145a57ce6
erfanemamverdi <erfanemamverdi9235@gmail.com>
2021-05-05 16:59:45 +0430

    ip funcs

---
+++ ping.py
@@ -0,0 +1,20 @@
+import socket
+
+def is_valid_ip4_address(addr):
+    parts = addr.split(".")
+    if not len(parts) == 4:
+        return False
+    for part in parts:
+        try:
+            number = int(part)
+        except ValueError:
+            return False
+        if number > 255 or number < 0:
+            return False
+    return True
+
+
+def to_ip(addr):
+    if is_valid_ip4_address(addr):
+        return addr
+    return socket.gethostbyname(addr)
